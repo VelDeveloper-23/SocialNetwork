cf37603d8c5f7b65a36350b15ac8ea4db1e48be8
VelDeveloper <zaricveljko365@gmail.com>
2023-03-14 19:55:35 +0100

    radi

--- js/User.js
+++ js/User.js
@@ -31,11 +31,36 @@ class User {
       });
   }
 
+  async get(user_id) {
+    let api_url = this.api_url + "/users/" + user_id;
+
+    let response = await fetch(api_url);
+    let data = await response.json();
+
+    return data;
+  }
+
   login() {
     fetch(this.api_url + "/users")
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
+        let login_successful = 0;
+        data.forEach((db_user) => {
+          if (
+            db_user.email === this.email &&
+            db_user.password === this.password
+          ) {
+            let session = new Session();
+            session.user_id = db_user.id;
+            session.startSession();
+            let login_successful = 1;
+            window.location.href = "hexa.html";
+          }
+        });
+
+        if (login_successful === 0) {
+          alert("Pogresan email ili lozinka");
+        }
       });
   }
 }
--- js/hexa.js
+++ js/hexa.js
@@ -1,8 +1,23 @@
 let session = new Session();
-session = session.getSession();
+session_id = session.getSession();
 
-if (session !== "") {
-  alert("Ulogovan si");
+if (session_id !== "") {
+  async function populateUserData() {
+    let user = new User();
+    user = await user.get(session_id);
+
+    document.querySelector("#username").innerText = user["username"];
+    document.querySelector("#email").innerText = user["email"];
+  }
+
+  populateUserData();
 } else {
   window.location.href = "/";
 }
+
+document.querySelector("#logout").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  session.destroySession();
+  window.location.href = "/";
+});
