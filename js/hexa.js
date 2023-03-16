095014d58ef6feb81af527f3ac63291a94d9d462
VelDeveloper <zaricveljko365@gmail.com>
2023-03-16 20:23:34 +0100

    jos malo

--- js/hexa.js
+++ js/hexa.js
@@ -58,12 +58,111 @@ document.querySelector("#postForm").addEventListener("submit", (e) => {
 
   async function createPost() {
     let content = document.querySelector("#postContent").value;
+    document.querySelector("#postContent").value = "";
     let post = new Post();
     post.post_content = content;
     post = await post.create();
 
-    console.log(post);
+    let current_user = new User();
+    current_user = await current_user.get(session_id);
+
+    let delete_post_html = "";
+
+    if (session_id === post.user_id) {
+      delete_post_html =
+        '<button class="remove-btn" onclick="removeMyPosts(this)">Remove</button>';
+    }
+
+    document.querySelector(
+      "#allPostsWrapper"
+    ).innerHTML = `<div class='single-post' data-post_id='${post.id}'>
+          <div class='post-content'>${post.content}</div>
+
+          <div class="post-actions">
+            <p><b>Autor:</b> ${current_user.username}</p>
+            <div>
+            <button onclick="likePost(this)" class="likePostJS like-btn"><span>${post.likes}</span> Likes</button>
+            <button class="coment-btn" onclick="commentPost(this)">Comments</button>
+            ${delete_post_html}
+
+            </div>
+
+          </div>
+
+
+
+          <div class='post-comments'>
+          <form>
+            <input placeholder="Napisi komentar..." type="text">
+            <button onclick="commentPostSubmit(event)">Comment</button>              
+          </form>
+
+
+
+           </div>       
+          </div>`;
   }
 
   createPost();
 });
+
+async function getAllPosts() {
+  let all_posts = new Post();
+
+  all_posts = await all_posts.getAllPosts();
+
+  all_posts.forEach((post) => {
+    async function getPostUser() {
+      let user = new User();
+      user = await user.get(post.user_id);
+
+      let html = document.querySelector("#allPostsWrapper").innerHTML;
+
+      let delete_post_html = "";
+
+      if (session_id === post.user_id) {
+        delete_post_html =
+          '<button class="remove-btn" onclick="removeMyPosts(this)">Remove</button>';
+      }
+
+      document.querySelector("#allPostsWrapper").innerHTML =
+        `<div class='single-post' data-post_id='${post.id}'>
+      <div class='post-content'>${post.content}</div>
+
+      <div class="post-actions">
+        <p><b>Autor:</b> ${user.username}</p>
+        <div>
+        <button onclick="likePost(this)" class="likePostJS like-btn"><span>${post.likes}</span> Likes</button>
+        <button class="coment-btn" onclick="commentPost(this)">Comments</button>
+        ${delete_post_html}
+
+        </div>
+
+      </div>
+
+
+
+      <div class='post-comments'>
+      <form>
+        <input placeholder="Napisi komentar..." type="text">
+        <button onclick="commentPostSubmit(event)">Comment</button>              
+      </form>
+
+
+
+       </div>       
+      </div>` + html;
+    }
+    getPostUser();
+  });
+}
+
+getAllPosts();
+
+const commentPostSubmit = (event) => {};
+
+const removeMyPosts = (el) => {};
+
+const likePost = (el) => {};
+
+const commentPost = (el) => {};
